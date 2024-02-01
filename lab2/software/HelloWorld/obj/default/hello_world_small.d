bb8d9725a5f62c4ac7f4e52f588bf29fa7b09cab
Adam Eljaafari <adamtantalum@gmail.com>
2024-02-01 09:31:40 +0000

    update lab 3

---
+++ lab2/software/HelloWorld/obj/default/hello_world_small.d
@@ -0,0 +1,17 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../HelloWorld_bsp//HAL/inc/sys/alt_stdio.h \
+ ../HelloWorld_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../HelloWorld_bsp//HAL/inc/io.h ../HelloWorld_bsp//HAL/inc/alt_types.h \
+ ../HelloWorld_bsp/system.h ../HelloWorld_bsp/linker.h
+
+../HelloWorld_bsp//HAL/inc/sys/alt_stdio.h:
+
+../HelloWorld_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../HelloWorld_bsp//HAL/inc/io.h:
+
+../HelloWorld_bsp//HAL/inc/alt_types.h:
+
+../HelloWorld_bsp/system.h:
+
+../HelloWorld_bsp/linker.h:
