27f2d39781533ad5025caf2166d3c741c5082bb2
Adam Eljaafari <adamtantalum@gmail.com>
2024-02-01 10:03:09 +0000

    update lab 3

---
+++ lab3/src/Golden_Top/software/accelerometer_c/obj/default/led_acceleromter_main.d
@@ -0,0 +1,51 @@
+obj/default/led_acceleromter_main.o: led_acceleromter_main.c \
+ ../accelerometer_c_bsp/system.h ../accelerometer_c_bsp/linker.h \
+ ../accelerometer_c_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h \
+ ../accelerometer_c_bsp//HAL/inc/alt_types.h \
+ ../accelerometer_c_bsp//HAL/inc/sys/alt_dev.h \
+ ../accelerometer_c_bsp//HAL/inc/sys/alt_llist.h \
+ ../accelerometer_c_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../accelerometer_c_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h \
+ ../accelerometer_c_bsp//HAL/inc/io.h \
+ ../accelerometer_c_bsp//HAL/inc/alt_types.h \
+ ../accelerometer_c_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../accelerometer_c_bsp//drivers/inc/altera_avalon_timer.h \
+ ../accelerometer_c_bsp//HAL/inc/sys/alt_warning.h \
+ ../accelerometer_c_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../accelerometer_c_bsp//HAL/inc/sys/alt_irq.h \
+ ../accelerometer_c_bsp//HAL/inc/nios2.h \
+ ../accelerometer_c_bsp//HAL/inc/priv/alt_legacy_irq.h
+
+../accelerometer_c_bsp/system.h:
+
+../accelerometer_c_bsp/linker.h:
+
+../accelerometer_c_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h:
+
+../accelerometer_c_bsp//HAL/inc/alt_types.h:
+
+../accelerometer_c_bsp//HAL/inc/sys/alt_dev.h:
+
+../accelerometer_c_bsp//HAL/inc/sys/alt_llist.h:
+
+../accelerometer_c_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../accelerometer_c_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h:
+
+../accelerometer_c_bsp//HAL/inc/io.h:
+
+../accelerometer_c_bsp//HAL/inc/alt_types.h:
+
+../accelerometer_c_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../accelerometer_c_bsp//drivers/inc/altera_avalon_timer.h:
+
+../accelerometer_c_bsp//HAL/inc/sys/alt_warning.h:
+
+../accelerometer_c_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../accelerometer_c_bsp//HAL/inc/sys/alt_irq.h:
+
+../accelerometer_c_bsp//HAL/inc/nios2.h:
+
+../accelerometer_c_bsp//HAL/inc/priv/alt_legacy_irq.h:
