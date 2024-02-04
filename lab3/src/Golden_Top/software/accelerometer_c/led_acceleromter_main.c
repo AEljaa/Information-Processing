f8ec8ea7bffe55f84af6ee3ada5709d52a6c751a
Adam Eljaafari <adamtantalum@gmail.com>
2024-02-04 20:49:59 +0000

    lab3 no chal done

--- lab3/src/Golden_Top/software/accelerometer_c/led_acceleromter_main.c
+++ lab3/src/Golden_Top/software/accelerometer_c/led_acceleromter_main.c
@@ -12,16 +12,41 @@
 alt_8 pwm = 0;
 alt_u8 led;
 int level;
+#define taps 5
+
+float coeffs[taps] = {0};
+alt_32 samples[taps] = {0};
+
+void initFIR(){
+    for(int i=0;i<taps;i++){
+        samples[i]=0;
+        coeffs[i]=1.0/taps;
+    }
+}
 
 void led_write(alt_u8 led_pattern) {
     IOWR(LED_BASE, 0, led_pattern);
 }
 
+alt_32 FIRLP(alt_32 newestVal) {
+    alt_32 avg=0;
+    for(int i=0;i<taps-1;i++){
+        samples[i+1]=samples[i];
+    }
+    samples[0]=newestVal;
+     
+    for(int i=0;i<taps;i++){
+        avg += coeffs[i]*samples[i];
+    }
+    return avg;
+}
+
 void convert_read(alt_32 acc_read, int * level, alt_u8 * led) {
     acc_read += OFFSET;
-    alt_u8 val = (acc_read >> 6) & 0x07;
+    alt_32 avg=FIRLP(acc_read);//moving avg
+    alt_u8 val = (avg >> 6) & 0x07;
     * led = (8 >> val) | (8 << (8 - val));
-    * level = (acc_read >> 1) & 0x1f;
+    * level = (avg >> 1) & 0x1f;
 }
 
 void sys_timer_isr() {
@@ -66,12 +91,13 @@ int main() {
     if (acc_dev == NULL) { // if return 1, check if the spi ip name is "accelerometer_spi"
         return 1;
     }
+    initFIR();
 
     timer_init(sys_timer_isr);
     while (1) {
 
         alt_up_accelerometer_spi_read_x_axis(acc_dev, & x_read);
-        // alt_printf("raw data: %x\n", x_read);
+        alt_printf("raw data: %x\n", x_read);
         convert_read(x_read, & level, & led);
 
     }
